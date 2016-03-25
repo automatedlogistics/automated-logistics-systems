e39abfe69625a9f00108c5197bdd30ad4218ea6b
Eric Defore <eric.defore@gmail.com>
2016-03-25 09:40:24 -0400

    Added full-width template for pages that may need it

---
+++ template-full_width.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: Full-Width
+ *
+ * @since 0.1.0
+ * @package automated-logistics-systems
+ */
+
+// Don't load directly
+if ( ! defined( 'ABSPATH' ) ) {
+    die;
+}
+
+get_header();
+
+the_post();
+?>
+
+<section id="page-<?php the_ID(); ?>" <?php body_class( array( 'page-content', 'full-width' ) ); ?>>
+    <div class="row">
+        <div class="small-12 columns">
+
+            <?php if ( has_post_thumbnail() ) : ?>
+            <div class="page-image">
+                <?php the_post_thumbnail( 'full' ); ?>
+            </div>
+            <?php endif; ?>
+            
+            <h1 class="page-title"><?php the_title(); ?></h1>
+
+            <div class="page-copy">
+                <?php the_content(); ?>
+            </div>
+            
+        </div>
+
+    </div>
+</section>
+
+<?php
+get_footer();
