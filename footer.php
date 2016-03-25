3fef3c15a2478d3343b7512f2ea7a3a26a6b3d84
Eric Defore <eric.defore@gmail.com>
2016-03-25 11:51:17 -0400

    Added Social Icons and Copyright Info to Footer

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <footer id="site-footer">
     
-    <div class = "row">
+    <div class="footer-widgets row">
 
         <?php
         $footer_columns = get_theme_mod( 'als_footer_columns', 4 );
@@ -32,6 +32,66 @@ if ( ! defined( 'ABSPATH' ) ) {
         }
         ?>
 
+    </div>
+    
+    <div class="row footer-connect text-center">
+        
+        <div class="small-12 columns">
+            
+            <?php echo sprintf( __( 'Copyright &copy; %d', THEME_ID ), date( 'Y' ) ); ?>
+            <?php _e( ' | All Rights Reserved | ', THEME_ID ); ?>
+            <?php echo sprintf( __( '%s, inc.', THEME_ID ), get_bloginfo( 'name' ) ); ?>
+        
+        </div>
+        
+        <div class="small-12 columns">
+        
+            <?php 
+                $social_accounts = array(
+                    'als_facebook' => array( 
+                        'label' => 'Facebook',
+                        'icon' => 'facebook-square',
+                    ),
+                    'als_twitter' => array( 
+                        'label' => 'Twitter',
+                        'icon' => 'twitter-square',
+                    ),
+                    'als_pinterest' => array( 
+                        'label' => 'Pinterest',
+                        'icon' => 'pinterest-square',
+                    ),
+                    'als_linkedin' => array( 
+                        'label' => 'LinkedIn',
+                        'icon' => 'linkedin-square',
+                    ),
+                    'als_instagram' => array( 
+                        'label' => 'Instagram',
+                        'icon' => 'instagram',
+                    ),
+                );
+
+            foreach ( $social_accounts as $key => $social ) :
+
+                if ( get_theme_mod( $key, '' ) !== '' ) : ?>
+
+                    <a class="social-icon" href="<?php echo get_theme_mod( $key, '' ); ?>" title="<?php echo sprintf( __( 'Connect with us on %s', THEME_ID ), $social['label'] ); ?>">
+                        <span class="fa fa-<?php echo $social['icon']; ?>"></span>
+                    </a>
+
+                <?php endif;
+
+            endforeach;
+
+            if ( get_theme_mod( 'als_rss_show', false ) === true ) : ?>
+
+                <a class="social-icon" href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php _e( 'Get our RSS Feed', THEME_ID ); ?>">
+                    <span class="fa fa-rss-square"></span>
+                </a>
+
+            <?php endif; ?>
+
+        </div>
+        
     </div>
 
 </footer>
