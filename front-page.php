5050560a6ce9f5bb2e2d56e278a3c8e1909c1879
Eric Defore <eric.defore@gmail.com>
2016-03-28 15:15:56 -0400

    Added mini-nav to Homepage. Referencing #1

--- front-page.php
+++ front-page.php
@@ -10,17 +10,48 @@ get_header();
 the_post();
 ?>
 
-<section id="page-<?php the_ID(); ?>" <?php body_class( array( 'page-content', 'home-page' ) ); ?>>
+<section id="mini-nav">
+    
     <div class="row">
-        <div class="small-12 columns">
-
-            <div class="page-copy">
-                <?php the_content(); ?>
+        
+        <div class="small-12 medium-6 medium-centered columns">
+            <div class="row">
+                
+                <div class="small-12 medium-4 columns text-center">
+                    <a href="/shippers">
+                        <span class="fa-stack fa-4x">
+                            <span class="fa fa-stack-2x fa-circle"></span>
+                            <span class="fa fa-stack fa-inverse fa-cube"></span>
+                        </span><br />
+                        <h6>Shippers</h6>
+                    </a>
+                </div>
+                
+                <div class="small-12 medium-4 columns text-center">
+                    <a href="/carriers">
+                        <span class="fa-stack fa-4x">
+                            <span class="fa fa-stack-2x fa-circle"></span>
+                            <span class="fa fa-stack fa-inverse fa-circle-o"></span>
+                        </span><br />
+                        <h6>Carriers</h6>
+                    </a>
+                </div>
+                
+                <div class="small-12 medium-4 columns text-center">
+                    <a href="/careers">
+                        <span class="fa-stack fa-4x">
+                            <span class="fa fa-stack-2x fa-circle"></span>
+                            <span class="fa fa-stack fa-inverse fa-suitcase"></span>
+                        </span><br />
+                        <h6>Careers</h6>
+                    </a>
+                </div>
+                
             </div>
-            
         </div>
-
+        
     </div>
+    
 </section>
 
 <section id="home-timeline">
@@ -29,66 +60,70 @@ the_post();
     
             $index = 0; ?>
     
-            <div class="row">
-                <div class="timeline-dot"></div>
-            </div>
-    
-            <?php while ( have_rows( 'home_timeline' ) ) : the_row(); ?>
+            <div class="container">
     
                 <div class="row">
-
-                    <div class="small-12 medium-6 columns text-right">
-                        
-                        <?php if ( $index % 2 == 0 ) :
-                        
-                            if ( get_sub_field( 'image' ) ) :
-                        
-                                echo wp_get_attachment_image( get_sub_field( 'image' ), 'timeline-image' );
-                        
-                            endif;
-                        
-                        else : ?>
-                        
-                            <h3><?php echo get_sub_field( 'title' ); ?></h3>
-                            <?php echo apply_filters( 'the_content', get_sub_field( 'main_text' ) ); ?>
-                            <a class="secondary button" href="<?php echo get_sub_field( 'button_link' ); ?>"><?php echo get_sub_field( 'button_text' ); ?></a>
-                        
-                        <?php endif; ?>
-                    
-                    </div>
-                    
                     <div class="timeline-dot"></div>
+                </div>
 
-                    <div class="small-12 medium-6 columns text-left">
-                        
-                        <?php if ( $index % 2 == 0 ) : ?>
-                        
-                            <h3><?php echo get_sub_field( 'title' ); ?></h3>
-                            <?php echo apply_filters( 'the_content', get_sub_field( 'main_text' ) ); ?>
-                            <a class="secondary button" href="<?php echo get_sub_field( 'button_link' ); ?>"><?php echo get_sub_field( 'button_text' ); ?></a>
-                        
-                        <?php else :
-                        
-                            if ( get_sub_field( 'image' ) ) :
-                        
-                                echo wp_get_attachment_image( get_sub_field( 'image' ), 'timeline-image' );
-                        
-                            endif;
-                        
-                        endif; ?>
-                    
+                <?php while ( have_rows( 'home_timeline' ) ) : the_row(); ?>
+
+                    <div class="row">
+
+                        <div class="small-12 medium-6 columns text-right">
+
+                            <?php if ( $index % 2 == 0 ) :
+
+                                if ( get_sub_field( 'image' ) ) :
+
+                                    echo wp_get_attachment_image( get_sub_field( 'image' ), 'timeline-image' );
+
+                                endif;
+
+                            else : ?>
+
+                                <h3><?php echo get_sub_field( 'title' ); ?></h3>
+                                <?php echo apply_filters( 'the_content', get_sub_field( 'main_text' ) ); ?>
+                                <a class="secondary button" href="<?php echo get_sub_field( 'button_link' ); ?>"><?php echo get_sub_field( 'button_text' ); ?></a>
+
+                            <?php endif; ?>
+
+                        </div>
+
+                        <div class="timeline-dot"></div>
+
+                        <div class="small-12 medium-6 columns text-left">
+
+                            <?php if ( $index % 2 == 0 ) : ?>
+
+                                <h3><?php echo get_sub_field( 'title' ); ?></h3>
+                                <?php echo apply_filters( 'the_content', get_sub_field( 'main_text' ) ); ?>
+                                <a class="secondary button" href="<?php echo get_sub_field( 'button_link' ); ?>"><?php echo get_sub_field( 'button_text' ); ?></a>
+
+                            <?php else :
+
+                                if ( get_sub_field( 'image' ) ) :
+
+                                    echo wp_get_attachment_image( get_sub_field( 'image' ), 'timeline-image' );
+
+                                endif;
+
+                            endif; ?>
+
+                        </div>
+
                     </div>
 
+                <?php 
+
+                $index++;
+
+                endwhile; ?>
+
+                <div class="row">
+                    <div class="timeline-dot"></div>
                 </div>
-        
-            <?php 
-    
-            $index++;
-    
-            endwhile; ?>
-    
-            <div class="row">
-                <div class="timeline-dot"></div>
+                
             </div>
         
         <?php endif; ?>
