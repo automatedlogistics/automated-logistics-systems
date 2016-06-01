7f15e154e0da067877c4eb19b5d7d254421a3c45
Eric Defore <eric.defore@gmail.com>
2016-06-01 09:31:52 -0400

    Added Content Block Sections for Connect Page. Closes #68

--- template-connect.php
+++ template-connect.php
@@ -35,7 +35,7 @@ the_post();
     
     <div class="row">
         
-        <div class="small-12 medium-9 columns">
+        <div class="small-12 medium-8 columns">
 
             <div class="page-copy">
                 <?php the_content(); ?>
@@ -51,58 +51,77 @@ the_post();
             
         </div>
         
-        <div class="small-12 medium-3 columns">
+        <div class="small-12 medium-4 columns sidebar-cta">
             
             <?php 
-                $social_accounts = array(
-                    'als_facebook' => array( 
-                        'label' => 'Facebook',
-                        'icon' => 'facebook-square',
-                    ),
-                    'als_twitter' => array( 
-                        'label' => 'Twitter',
-                        'icon' => 'twitter-square',
-                    ),
-                    'als_pinterest' => array( 
-                        'label' => 'Pinterest',
-                        'icon' => 'pinterest-square',
-                    ),
-                    'als_linkedin' => array( 
-                        'label' => 'LinkedIn',
-                        'icon' => 'linkedin-square',
-                    ),
-                    'als_instagram' => array( 
-                        'label' => 'Instagram',
-                        'icon' => 'instagram',
-                    ),
-                );
+            
+            $social_accounts = array(
+                'als_facebook' => array( 
+                    'label' => 'Facebook',
+                    'icon' => 'facebook-square',
+                ),
+                'als_twitter' => array( 
+                    'label' => 'Twitter',
+                    'icon' => 'twitter-square',
+                ),
+                'als_pinterest' => array( 
+                    'label' => 'Pinterest',
+                    'icon' => 'pinterest-square',
+                ),
+                'als_linkedin' => array( 
+                    'label' => 'LinkedIn',
+                    'icon' => 'linkedin-square',
+                ),
+                'als_instagram' => array( 
+                    'label' => 'Instagram',
+                    'icon' => 'instagram',
+                ),
+            ); ?>
+            
+            <div class="cta-block">
 
-            foreach ( $social_accounts as $key => $social ) :
+                <?php foreach ( $social_accounts as $key => $social ) :
 
-                if ( get_theme_mod( $key, '' ) !== '' ) : ?>
+                    if ( get_theme_mod( $key, '' ) !== '' ) : ?>
 
-                    <a class="social-icon" href="<?php echo get_theme_mod( $key, '' ); ?>" target="_blank" title="<?php echo sprintf( __( 'Connect with us on %s', THEME_ID ), $social['label'] ); ?>">
-                        <span class="fa fa-<?php echo $social['icon']; ?>"></span>
+                        <a class="social-icon" href="<?php echo get_theme_mod( $key, '' ); ?>" target="_blank" title="<?php echo sprintf( __( 'Connect with us on %s', THEME_ID ), $social['label'] ); ?>">
+                            <span class="fa fa-<?php echo $social['icon']; ?>"></span>
+                        </a>
+
+                    <?php endif;
+
+                endforeach;
+
+                if ( get_theme_mod( 'als_rss_show', false ) === true ) : ?>
+
+                    <a class="social-icon" href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php _e( 'Get our RSS Feed', THEME_ID ); ?>">
+                        <span class="fa fa-rss-square"></span>
                     </a>
 
-                <?php endif;
+                <?php endif; ?>
 
-            endforeach;
+                <?php echo apply_filters( 'the_content', get_theme_mod( 'als_address', "3517 Scheele Drive\nJackson, MI 49202" ) ); ?>
 
-            if ( get_theme_mod( 'als_rss_show', false ) === true ) : ?>
+                <iframe class="google-map" frameborder="0" scrolling="no" src="//maps.google.com/maps?&q=<?php echo get_theme_mod( 'als_address', urlencode( "3517 Scheele Drive\nJackson, MI 49202" ) ); ?>&output=embed"></iframe>
 
-                <a class="social-icon" href="<?php bloginfo( 'rss2_url' ); ?>" title="<?php _e( 'Get our RSS Feed', THEME_ID ); ?>">
-                    <span class="fa fa-rss-square"></span>
-                </a>
-
-            <?php endif; ?>
+                <?php echo sprintf( __( 'Phone: %s', THEME_ID ), get_phone_number_link( get_theme_mod( 'als_phone_number', '1-800-551-9399' ) ) ); ?><br />
+                <?php echo sprintf( __( 'Fax: %s', THEME_ID ), get_phone_number_link( get_theme_mod( 'als_fax_number', '1-888-764-6225' ) ) ); ?><br />
+                
+            </div>
             
-            <?php echo apply_filters( 'the_content', get_theme_mod( 'als_address', "3517 Scheele Drive\nJackson, MI 49202" ) ); ?>
+            <?php if ( have_rows( 'connect_sidebar_blocks' ) ) : 
             
-            <iframe class="google-map" frameborder="0" scrolling="no" src="//maps.google.com/maps?&q=<?php echo get_theme_mod( 'als_address', urlencode( "3517 Scheele Drive\nJackson, MI 49202" ) ); ?>&output=embed"></iframe>
+                while ( have_rows( 'connect_sidebar_blocks' ) ) : the_row(); ?>
             
-            <?php echo sprintf( __( 'Phone: %s', THEME_ID ), get_phone_number_link( get_theme_mod( 'als_phone_number', '1-800-551-9399' ) ) ); ?><br />
-            <?php echo sprintf( __( 'Fax: %s', THEME_ID ), get_phone_number_link( get_theme_mod( 'als_fax_number', '1-888-764-6225' ) ) ); ?><br />
+                    <div class="cta-block">
+            
+                        <?php echo apply_filters( 'the_content', get_sub_field( 'block' ) ); ?>
+                        
+                    </div>
+            
+                <?php endwhile;
+            
+            endif; ?>
         
         </div>
         
