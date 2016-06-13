0b1d3df861582c06ab8020129cab11e5548b69c3
Eric Defore <eric.defore@gmail.com>
2016-06-13 08:42:52 -0400

    No flashing of the Countdown overlay on Page Load

--- als-countdown/overlay.php
+++ als-countdown/overlay.php
@@ -1,3 +1,5 @@
+<div id="countdown-overlay-previous" style="background-color: rgba(0, 87, 144, 0.9);"></div>
+
 <div id="countdown-overlay" class="overlay" data-path-to="m 0,0 1439.999975,0 0,805.99999 -1439.999975,0 z">
 
     <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 1440 806" preserveAspectRatio="none">
