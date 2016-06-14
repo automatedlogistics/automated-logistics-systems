b574a09854c3a69bd27b520d1bbd42370fc02f92
Eric Defore <eric.defore@gmail.com>
2016-06-14 10:03:45 -0400

    Added Google Analytics Tracking Code

--- functions.php
+++ functions.php
@@ -1206,3 +1206,26 @@ function als_staff_save_bulk_edit_data() {
     wp_die();
     
 }
+
+/**
+ * Google Analyitcs Script
+ * 
+ * @since 1.0
+ * 
+ */
+add_action( 'wp_footer', function() { ?>
+    
+    <script>
+        
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-11042518-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
+<?php
+} );
